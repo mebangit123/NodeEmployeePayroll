fd965a0ebb331bce6eee3f05f3883ecc0e43e5cd
mebangit123 <mebannongrum333@gmail.com>
2021-06-01 21:40:31 +0530

    Merging UC2 to main branch

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,15 +1,16 @@
 class EmployeePayrollData
 {
-	id;
 	salary;
 	gender;
 	startDate;
 
-	constructor(id, name, salary)
+	constructor(...params)
 	{
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
+		this.id = params[0];
+		this.name = params[1];
+		this.salary = params[2];
+		this.gender = params[3];
+		this.startDate = params[4];
 	}
 	
 	get name() { return this._name; }
@@ -20,8 +21,15 @@ class EmployeePayrollData
 
 	toString() 
 	{
-		return "id= " + this.id + ", name= "+this.name+ ", salary= "+ this.salary;
+		const option = { year: 'numeric', month: 'long', day: 'numeric' };
+		const empDate = this.startDate === undefined ? "undefined" :
+							 this.startDate.toLocaleDateString("en-US", option);
+		return "id= " + this.id + ", name= "+this.name+  ", salary= "+ this.salary + ", "+
+					"gender= "+this.gender+ ", startDate= "+ empDate;
 	}
 }
 let employeePayrollData = new EmployeePayrollData(1, 'Mark', 3000);
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(2, 'Terisa', 3000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
