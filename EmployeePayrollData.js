840f70bfd09b18a160ed8e3570fc7b9df6972566
mebangit123 <mebannongrum333@gmail.com>
2021-06-01 21:35:00 +0530

    Create employee payroll Data

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,4 +1,27 @@
 class EmployeePayrollData
 {
+	id;
+	salary;
+	gender;
+	startDate;
 
+	constructor(id, name, salary)
+	{
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	get name() { return this._name; }
+	set name(name)
+	{
+		this._name = name;
+	}
+
+	toString() 
+	{
+		return "id= " + this.id + ", name= "+this.name+ ", salary= "+ this.salary;
+	}
 }
+let employeePayrollData = new EmployeePayrollData(1, 'Mark', 3000);
+console.log(employeePayrollData.toString());
