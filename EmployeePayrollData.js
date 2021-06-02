00930b675857eff25b67bc7afc5df6eacd6f9ba8
mebangit123 <mebannongrum333@gmail.com>
2021-06-02 12:39:56 +0530

    Added: Regex validation

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,9 +1,5 @@
 class EmployeePayrollData
 {
-	salary;
-	gender;
-	startDate;
-
 	constructor(...params)
 	{
 		this.id = params[0];
@@ -13,14 +9,41 @@ class EmployeePayrollData
 		this.startDate = params[4];
 	}
 	
+    get id() { return this._id }
+    set id(id)
+    {
+        if(id < 0)
+            throw 'ID: Enter positive integer'
+        else this._id = id
+    }
+
 	get name() { return this._name; }
 	set name(name)
 	{
 		let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
 		if(nameRegex.test(name))
 			this._name = name;
-		else throw 'Name is Incorrect!';
+		else {
+            throw 'Name is Incorrect!'
+        }
 	}
+
+    get salary() { return this._salary }
+    set salary(salary)
+    {
+        if(salary < 0 )
+            throw 'Salary: Enter positive integer'
+        else this._salary = salary
+    }
+
+	get gender() { return this._gender }
+    set gender(gender)
+    {
+		let genderRegex = RegExp('^[MF]{1}$')
+        if(genderRegex.test(gender) )
+		this._gender = gender
+		else throw 'Gender Not match'
+    }
 
 	toString() 
 	{
@@ -31,16 +54,12 @@ class EmployeePayrollData
 					"gender= "+this.gender+ ", startDate= "+ empDate;
 	}
 }
-let employeePayrollData = new EmployeePayrollData(1, 'Mark', 3000);
-console.log(employeePayrollData.toString());
+
 try
 {
-	employeePayrollData.name = "john";
-	console.log(employeePayrollData.toString());
-}
-catch(e)
-{
-	console.error(e);
-}
-let newEmployeePayrollData = new EmployeePayrollData(2, 'Terisa', 3000, "F", new Date());
+	let newEmployeePayrollData = new EmployeePayrollData(2, 'Terisa', 3000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
+}catch(e)
+{
+	console.error(e)
+}
